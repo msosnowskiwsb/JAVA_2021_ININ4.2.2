9f37cebb7af7c61b4937043a56813fcf8263fe9d
Mateusz Sosnowski <msosnowski@wsb.gda.pl>
2021-02-28 14:24:33 +0100

    Loop - print humans

--- src/pl/gda/wsb/Main.java
+++ src/pl/gda/wsb/Main.java
@@ -1,5 +1,7 @@
 package pl.gda.wsb;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -18,7 +20,17 @@ public class Main {
         System.out.println(human1.firstName);
 
         Animal animalWithMaxWeight = animal1.weight > animal2.weight ? animal1 : animal2;
-        System.out.println("The heaviest animal is " + animalWithMaxWeight.name + ". Its weight is: " +
-                animalWithMaxWeight.weight + " kg.");
+        System.out.println("The heaviest animal is " + animalWithMaxWeight.name + ". Its weight is: " + animalWithMaxWeight.weight + " kg.");
+
+        System.out.println("\n------------------------\n");
+
+        ArrayList<Human> humanslist = new ArrayList<>();
+        humanslist.add(human1);
+        humanslist.add(human2);
+
+        for (Human human : humanslist){
+            System.out.println(human.firstName);
+        }
+
     }
 }
